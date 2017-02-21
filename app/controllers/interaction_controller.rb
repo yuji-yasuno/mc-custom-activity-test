f045a54f8904574fc5f5a1b751c27bfb299722e5
楊野勇智 <yuji-1124-1984@Mac-Pro.local>
2017-02-22 00:28:02 +0900

    commit 2

--- app/controllers/interaction_controller.rb
+++ app/controllers/interaction_controller.rb
@@ -1,21 +1,21 @@
 class InteractionController < ApplicationController
   def execute
-    render text: 'OK', status: 200
+    render :text => 'OK', :status => 200
   end
 
   def save
-    render text: 'OK', status: 200
+    render :text => 'OK', :status => 200
   end
 
   def publish
-    render text: 'OK', status: 200
+    render :text => 'OK', :status => 200
   end
 
   def validate
-    render text: 'OK', status: 200
+    render :text => 'OK', :status => 200
   end
 
   def edit
-    render text: 'OK', status: 200
+    render :text => 'OK', :status => 200
   end
 end
