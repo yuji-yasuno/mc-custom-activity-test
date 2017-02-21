ecb5c8a5d3871c9ca1c503d686a87b4462d1c3f4
楊野勇智 <yuji-1124-1984@Mac-Pro.local>
2017-02-22 00:40:16 +0900

    commit 4

--- app/controllers/interaction_controller.rb
+++ app/controllers/interaction_controller.rb
@@ -1,21 +1,22 @@
 class InteractionController < ApplicationController
+  protect_from_forgery with: :null_session
   def execute
-
+    render text: 'OK', status: 200
   end
 
   def save
-
+    render text: 'OK', status: 200
   end
 
   def publish
-
+    render text: 'OK', status: 200
   end
 
   def validate
-
+    render text: 'OK', status: 200
   end
 
   def edit
-
+    render text: 'OK', status: 200
   end
 end
