bc066e308ebb45daa7bd179ccd0bc6c31e7bad3c
楊野勇智 <yuji-1124-1984@Mac-Pro.local>
2017-02-22 21:06:32 +0900

    create endpoints

--- app/controllers/interaction_controller.rb
+++ app/controllers/interaction_controller.rb
@@ -1,22 +1,32 @@
 class InteractionController < ApplicationController
   protect_from_forgery with: :null_session
   def execute
+    log_json
     render text: 'OK', status: 200
   end
 
   def save
+    log_json
     render text: 'OK', status: 200
   end
 
   def publish
+    log_json
     render text: 'OK', status: 200
   end
 
   def validate
+    log_json
     render text: 'OK', status: 200
   end
 
   def edit
+    log_json
     render text: 'OK', status: 200
+  end
+
+  def log_json
+    json_request = JSON.parse request.body.read
+    logger.info("json: #{json_request}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'home/index'
+
   post '/execute', to: 'interaction#execute'
 
   post '/save', to: 'interaction#save'
