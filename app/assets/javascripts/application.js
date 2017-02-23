136fb070e875075753503034cfca6b3e9683a66a
楊野勇智 <yuji-1124-1984@Mac-Pro.local>
2017-02-23 23:37:54 +0900

    added all on envents of postmonger.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,32 +4,46 @@ require(['jquery', 'postmonger'], function ($, Postmonger) {
     console.log('connection: ');
     console.log(connection);
 
-    connection.on('initActivity', function () {
+    connection.on('initActivity', function (payload) {
         console.log('on initActivity');
+        console.log('payload:');
+        console.log(payload);
     });
 
     connection.on('requestedTokens', function (tokens) {
         console.log('on requestedTokens');
+        console.log('tokens:');
+        console.log(tokens);
     });
 
-    connection.on('requestedEndpoints', function () {
+    connection.on('requestedEndpoints', function (endpoints) {
         console.log('on requestedEndpoints');
+        console.log('endpoints:');
+        console.log(endpoints);
     });
 
     connection.on('clickedNext', function () {
-        console.log('on ');
-    });
-
-    connection.on('clickedBack', function () {
         console.log('on clickedNext');
     });
 
-    connection.on('requestedInteractionDefaults', function () {
-        console.log('on requestedInteractionDefaults');
+    connection.on('clickedBack', function () {
+        console.log('on clickedBack');
     });
 
-    connection.on('requestedInteractionDefaults', function () {
+    connection.on('gotoStep', function () {
+        console.log('on gotoStep');
+    });
+
+    connection.on('requestedInteractionDefaults', function (settings) {
         console.log('on requestedInteractionDefaults');
+        console.log('settings:');
+        console.log(settings);
+    });
+
+    connection.on('requestedInteraction', function (interaction) {
+        console.log('on requestedInteractionDefaults');
+        console.log('interaction:');
+        console.log(interaction);
     });
 
     $(document).ready(function() {
